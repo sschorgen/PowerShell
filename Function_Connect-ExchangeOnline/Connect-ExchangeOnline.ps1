7374b3bb6f7b759679285611b4e916e2b53966e2
sschorgen <register@schorgen.com>
2016-03-21 13:33:06 +1100

    Connect-ExchangeOnline Function
    Initial Commit

---
+++ Function_Connect-ExchangeOnline/Connect-ExchangeOnline.ps1
@@ -0,0 +1,45 @@
+Function Connect-ExchangeOnline {
+	
+	<#
+    .SYNOPSIS
+        Connect to your Exchange Online tenant
+    .DESCRIPTION
+        Connect to your Exchange Online tenant
+    .PARAMETER O365AdminLogin
+        Specify an Office 365 Administrator
+    .PARAMETER O365AdminPassword
+        Specify the Office 365 Administrator password
+    .EXAMPLE
+        .\Connect-ExchangeOnline.ps1 -O365AdminLogin "admin@mytenant.onmicrosoft.com" -O365AdminPassword "MYPASSWORD"
+        This will connect you to your Exchange Online Tenant and load the Exchange Online Cmdlets
+    .NOTES
+        Author : Sylver SCHORGEN
+        Blog : http://microsofttouch.fr/default/b/sylver
+        Created : 21 mar. 2016
+        Twitter : @sylver_schorgen
+#>
+	
+	param (
+		[Parameter(Mandatory=$true)]
+		[string]$O365AdminLogin;
+		[Parameter(Mandatory=$true)]
+		[string]$O365AdminPassword
+	)
+	
+	Write-Host
+	Write-Host
+	Write-Host "Connecting to Exchange Online ... " -NoNewLine
+	
+	Try {
+		$O365SecureAdminPassword = ConvertTo-SecureString -AsPlainText $O365AdminPassword -Force
+		$UserCredential  = New-Object System.Management.Automation.PSCredential ($O365AdminLogin,$O365SecureAdminPassword)
+		
+		$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection -ErrorAction Stop
+		Import-PSSession $Session
+		
+		Write-Host " DONE ! " -ForegroundColor Green
+	} Catch {
+		Write-Host " ERROR ! " -ForegroundColor Red
+	}
+	
+}
